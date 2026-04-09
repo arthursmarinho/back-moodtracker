073d55369008d6e0ef29d15c6623273c929e86b7
arthursmarinho <arthursnat@gmail.com>
2026-04-08 21:52:12 -0300

    feat: service do mood agora se comunica com o prisma e banco de dados

--- src/mood/mood.service.ts
+++ src/mood/mood.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PostMoodDto } from './dto/post-mood.dto';
+import { Mood } from 'src/generated/prisma/client';
 @Injectable()
-export class MoodService {}
+export class MoodService {
+  constructor(private prisma: PrismaService) {}
+
+  async createPost(dto: PostMoodDto): Promise<Mood> {
+    return this.prisma.mood.create({ 
+      data: {
+        ...dto
+      } 
+    });
+  }
+
+  async listPosts(): Promise<Mood[]> {
+    return this.prisma.mood.findMany({
+      orderBy: { publishedAt: 'desc' } 
+    });
+  }
+
+  async deletePost(id: number): Promise<Mood> {
+    return this.prisma.mood.delete({ 
+      where: { id } 
+    });
+  }
+} 
