295e87806fd7817c0716e4cd8c347b8cb0c71d8e
arthursmarinho <arthursnat@gmail.com>
2026-04-08 23:56:48 -0300

    feat: agora o filtro de datas vem do back

--- src/mood/mood.service.ts
+++ src/mood/mood.service.ts
@@ -2,27 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PostMoodDto } from './dto/post-mood.dto';
 import { Mood } from 'src/generated/prisma/client';
+
 @Injectable()
 export class MoodService {
   constructor(private prisma: PrismaService) {}
 
   async createPost(dto: PostMoodDto): Promise<Mood> {
-    return this.prisma.mood.create({ 
+    const { mood, ...rest } = dto;
+    return this.prisma.mood.create({
       data: {
-        ...dto
-      } 
+        ...rest,
+        moods: mood,
+        publishedAt: new Date(),
+      },
     });
   }
 
-  async listPosts(): Promise<Mood[]> {
+  async listPosts(startDate?: string, endDate?: string): Promise<Mood[]> {
     return this.prisma.mood.findMany({
-      orderBy: { publishedAt: 'desc' } 
+      where: {
+        publishedAt: {
+          gte: startDate ? new Date(startDate) : undefined,
+          lte: endDate ? new Date(endDate) : undefined,
+        },
+      },
+      orderBy: { publishedAt: 'desc' },
     });
   }
 
   async deletePost(id: number): Promise<Mood> {
-    return this.prisma.mood.delete({ 
-      where: { id } 
+    return this.prisma.mood.delete({
+      where: { id },
     });
   }
-} 
+}
