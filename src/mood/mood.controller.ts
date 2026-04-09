6470f7b505f74873122110c969fc80a61004e4a4
arthursmarinho <arthursnat@gmail.com>
2026-04-08 21:52:54 -0300

    feat: controller do mood se comunica com o service e rotas para get, delete e post foram criadas

--- src/mood/mood.controller.ts
+++ src/mood/mood.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Delete, Get, Param, ParseIntPipe} from '@nestjs/common';
+import { MoodService } from './mood.service';
+import { PostMoodDto } from './dto/post-mood.dto';
 
 @Controller('mood')
-export class MoodController {}
+export class MoodController {
+      constructor(private readonly moodService: MoodService) {}
+
+      @Post()
+  create(@Body() PostMoodDto: PostMoodDto) {
+    return this.moodService.createPost(PostMoodDto);
+  }
+
+    @Get()
+  list() {
+    return this.moodService.listPosts();
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.moodService.deletePost(id);
+  }
+
+}
